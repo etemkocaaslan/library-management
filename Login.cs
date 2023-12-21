410630eea546898d6b6296ec80aa1ec327d3a6e0
kasla <kasla@DESKTOP-SR937O1>
2023-12-21 00:14:12 -0800

    sql inj preventation

--- Login.cs
+++ Login.cs
@@ -1,27 +1,35 @@
+using System.Data.SqlClient;
+using System.Data;
+
 namespace WinFormsApp2
 {
     public partial class Login : Form
     {
-        int count = 0;
         public Login()
         {
             InitializeComponent();
+            Model.InitializeConnection("Data Source=DESKTOP-SR937O1;Initial Catalog=libmanag;Integrated Security=True");
         }
 
-        private void Bt1_Click(object sender, EventArgs e)
+        private async void Bt1_Click(object sender, EventArgs e)
         {
-            Model.InitializeConnection("Data Source=DESKTOP-SR937O1;Initial Catalog=libmanag;Integrated Security=True");
-            string query = "select * from library_person where username='" + tb1.Text + "' and password='" + tb2.Text + "'";
-            count = Convert.ToInt32(Model.ExecuteQuery(query).Rows.Count.ToString());
-            if (count == 0)
+            string query = "SELECT * from library_person WHERE username = @username and password = @password";
+            SqlParameter[] parameters = new SqlParameter[] 
             {
-                MessageBox.Show("username password does not match");
+                new SqlParameter("@username", tb1.Text),
+                new SqlParameter("Password", tb2.Text)
+            };
+            DataTable dataTable = await Task.Run(() => Model.ExecuteQuery(query, parameters));
+
+            if (dataTable.Rows.Count > 0)
+            {
+                this.Hide();
+                MDIuser mu = new();
+                mu.Show();
             }
             else
             {
-                this.Hide();
-                mdi_user mu = new();
-                mu.Show();
+                MessageBox.Show("username password does not match");
             }
         }
     }
