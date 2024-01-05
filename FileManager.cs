484221a7ab8df0db1c33b92a570d3b75cb72a193
kasla <kasla@DESKTOP-SR937O1>
2024-01-04 17:54:02 -0800

    Filemanager seperated from databasehelper

---
+++ FileManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+
+namespace WinFormsApp2
+{
+    internal static class FileManager
+    {
+        private static readonly string? image_path = ConfigurationManager.AppSettings["ImagePath"];
+        private static readonly OpenFileDialog openImageFileDialog = new()
+        {
+            Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp",
+            InitialDirectory = image_path,
+            FileName = ""
+        };
+
+        public static Bitmap? GetImage()
+        {
+            return (openImageFileDialog.ShowDialog() == DialogResult.OK) ? new Bitmap(openImageFileDialog.FileName) : null;
+        }
+    }
+}
