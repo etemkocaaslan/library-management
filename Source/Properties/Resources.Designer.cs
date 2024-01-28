8c09508894676898634eb6044a8ff21fd79ecf90
kasla <kasla@DESKTOP-SR937O1>
2024-01-27 17:12:11 -0800

    moved to Source/Helpers path

--- Source/Properties/Resources.Designer.cs
+++ Source/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LibraryManagement.Properties {
+namespace LibraryManagement.Source.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace LibraryManagement.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LibraryManagement.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LibraryManagement.Source.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
